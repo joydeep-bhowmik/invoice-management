de55a88c2b71dabca421785942fbb4127d65d933
joydeep-bhowmik <bhowmikj9432@gmail.com>
2026-03-15 00:10:57 +0530

    wip

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,18 @@
 
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\SvgController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/run-seeder', function (Request $request) {
+
+    if ($request->input('code') !== 8974) {
+        abort(404);
+    }
+    Artisan::command('migrate:fresh', function () {});
+    Artisan::command('db:seed', function () {});
+});
 
 Route::get('/', function () {
     return redirect(route('dashboard'));
@@ -14,6 +25,6 @@ Route::get('/invoices/{invoice}/pdf', [InvoiceController::class, 'download'])
     ->name('invoices.pdf');
 
 Route::get('/svg/render', [SvgController::class, 'render'])
-        ->name('svg.render');
+    ->name('svg.render');
 
 require __DIR__.'/settings.php';
